2af74667941918d6d200780df1ed1e0a3ba0d3c3
ONENAYOO_W <wittawat5552009@gmail.com>
2018-09-10 13:02:13 +0700

    first commit

---
+++ result.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="th">
+<head>
+  <title>ตุดตู่</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<table class ="table table-hover">
+<thead>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>email</th>
+<th>gender</th>
+<th>Age</th>
+<th>address</th>
+</tr>
+</thead>
+
+<tbody>
+
+<?php
+include("conn/phpmyconn.php");
+
+$sql = "SELECT * FROM userprofile";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result)>0){
+	while($row = mysqli_fetch_assoc($result)){
+?>
+<tr>
+<td><?php echo $row["userid"];?></td>
+<td><?php echo $row["name"];?></td>
+<td><?php echo $row["email"];?></td>
+<td><?php echo $row["age"];?></td>
+<td><?php echo $row["address"];?></td>
+<td><?php echo $row["gender"];?></td>
+</tr>
+
+<?php
+
+
+
+
+//echo "userid: " .$row["userid"]." - Name: " . $row["name"]." ".$row["email"]." ".$row["age"]." ".$row["address"]." ".$row["gender"]."<br>";	
+	}}
+else{
+	echo "0 result";
+}
+
+
+
+
+?>
+</tbody>
+</table>
+</body>
+
+</html>
